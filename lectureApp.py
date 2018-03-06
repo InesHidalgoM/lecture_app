81206473fc0d2a54447c33337ebd01645a63cfb5
Kate Marshall <katemarshall@eduroam-int-dhcp-97-39-187.ucl.ac.uk>
2018-03-06 20:04:44 +0000

    first commit

---
+++ lectureApp.py
@@ -0,0 +1,19 @@
+from flask import Flask , render_template, request
+from datetime import datetime
+
+app = Flask("My Flask App")
+
+
+@app.route("/") 
+def default_path():
+	return render_template ("index.html") #This 
+
+
+@app.route("/show_lectures", methods=["POST"])
+def read_form():
+	return render_template ("lectures.html", date=date, sign=sign, data=data) 
+	#return "All OK"
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
