88c0ebae30f5af35081ab26758b30d7366c16b17
Kate Marshall <katemarshall@eduroam-int-dhcp-97-240-243.ucl.ac.uk>
2018-03-12 18:57:35 +0000

    new commit

--- lectureApp.py
+++ lectureApp.py
@@ -1,6 +1,6 @@
 from flask import Flask , render_template, request
 from datetime import datetime
-from eventbrite import Eventbrite
+#from eventbrite import Eventbrite
 
 app = Flask("My Flask App")
 
@@ -16,12 +16,12 @@ def read_form():
 	#return "All OK"
 
 
-eventbrite = Eventbrite("5Q5HRS46AWMKIYGZGEAW")
-user = eventbrite.get_user()
-user["id"]
-63732380485
-user["name"]
-Amelie Johnson-Ferguson
+#eventbrite = Eventbrite("5Q5HRS46AWMKIYGZGEAW")
+#user = eventbrite.get_user()
+#user["id"]
+#63732380485
+#user["name"]
+#Amelie Johnson-Ferguson
 
 
 if __name__ == '__main__':
