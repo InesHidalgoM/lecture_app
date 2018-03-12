22e33afa020000ae56945fecce0df02fbf41037c
Kate Marshall <katemarshall@eduroam-int-dhcp-97-240-243.ucl.ac.uk>
2018-03-12 18:53:50 +0000

    progress...

--- lectureApp.py
+++ lectureApp.py
@@ -1,5 +1,6 @@
 from flask import Flask , render_template, request
 from datetime import datetime
+from eventbrite import Eventbrite
 
 app = Flask("My Flask App")
 
@@ -13,6 +14,14 @@ def default_path():
 def read_form():
 	return render_template ("lectures.html", date=date, sign=sign, data=data) 
 	#return "All OK"
+
+
+eventbrite = Eventbrite("5Q5HRS46AWMKIYGZGEAW")
+user = eventbrite.get_user()
+user["id"]
+63732380485
+user["name"]
+Amelie Johnson-Ferguson
 
 
 if __name__ == '__main__':
