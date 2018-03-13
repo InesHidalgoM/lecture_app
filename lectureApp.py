e4e2d27a83a8dd72175ec63469c5f94f97796e48
Kate Marshall <katemarshall@eduroam-int-dhcp-97-240-243.ucl.ac.uk>
2018-03-13 18:32:51 +0000

    new commit

--- lectureApp.py
+++ lectureApp.py
@@ -15,14 +15,5 @@ def read_form():
 	return render_template ("lectures.html", date=date, sign=sign, data=data) 
 	#return "All OK"
 
-
-#eventbrite = Eventbrite("5Q5HRS46AWMKIYGZGEAW")
-#user = eventbrite.get_user()
-#user["id"]
-#63732380485
-#user["name"]
-#Amelie Johnson-Ferguson
-
-
 if __name__ == '__main__':
 	app.run(debug=True)
