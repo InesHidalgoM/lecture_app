8129b8a4a72a81eefae1908b0c6a9d42362059e7
Kate Marshall <katemarshall@eduroam-int-dhcp-97-64-180.ucl.ac.uk>
2018-03-19 21:16:21 +0000

    new

--- lectureApp.py
+++ lectureApp.py
@@ -14,7 +14,7 @@ param = {
 }
 #print param["location.address"]
 
-lectures = eventbrite.event_search(**param)
+lectures = eventbrite.event_search(q="lecture")
 one = lectures["events"][9]
 
 app = Flask("My Flask App")
