4bac3912abcea8e674645c1047694e10de86b28c
Kate Marshall <katemarshall@eduroam-int-dhcp-97-64-180.ucl.ac.uk>
2018-03-19 22:07:22 +0000

    new

--- lectureApp.py
+++ lectureApp.py
@@ -8,15 +8,6 @@ user = eventbrite.get_user()
 user["id"]
 user["name"]
 
-@app.route("/get_my_ip", methods=["GET"])
-def get_my_ip():
-    return jsonify({"ip": request.remote_addr}), 200
-#here = request.remote_addr
-
-#response = requests.get("https://ipinfo.io/{}/json" .format(here))
-#response.json()
-#location = here[loc]
-
 param = {
     "q":"lecture",
     "location.address": "Edinburgh"
@@ -32,15 +23,23 @@ six = lectures["events"][6]
 
 app = Flask("My Flask App")
 
-
 @app.route("/")
 def default_path():
-    return render_template ("index.html", name1=(one["name"]["text"]), content1=(one["description"]["text"]), name2=(two["name"]["text"]), content2=(two["description"]["text"]), name3=(three["name"]["text"]), content3=(three["description"]["text"]), name4=(four["name"]["text"]), content4=(four["description"]["text"]), name5=(five["name"]["text"]), content5=(five["description"]["text"]),name6=(six["name"]["text"]), content6=(six["description"]["text"])) #This
+    return render_template ("index.html", name1=(one["name"]["text"]), content1=(one["description"]["text"]), name2=(two["name"]["text"]), content2=(two["description"]["text"]), name3=(three["name"]["text"]), content3=(three["description"]["text"]), name4=(four["name"]["text"]), content4=(four["description"]["text"]), name5=(five["name"]["text"]), content5=(five["description"]["text"]),name6=(six["name"]["text"]), content6=(six["description"]["text"]))
 
-@app.route("/show_lectures", methods=["POST"])
-def read_form():
-	return render_template ("lectures.html", date=date, sign=sign, data=data)
-	return "All OK"
+@app.route("/get_my_ip", methods=["GET"])
+def get_my_ip():
+    return jsonify({"ip": request.remote_addr}), 200
+#here = request.remote_addr
+
+#response = requests.get("https://ipinfo.io/{}/json" .format(here))
+#response.json()
+#location = here[loc]
+
+#@app.route("/show_lectures", methods=["POST"])
+#def read_form():
+#return render_template ("lectures.html", date=date, sign=sign, data=data)
+#return "All OK"
 
 if __name__ == '__main__':
 	app.run(debug=True)
